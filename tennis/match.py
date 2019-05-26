de396f943a725a527a4754487f6e1165113c6511
sourabh <acumen.sourabh@gmail.com>
2019-05-26 22:44:38 +1000

    added functionality to calculate score

--- tennis/match.py
+++ tennis/match.py
@@ -16,29 +16,67 @@ class Match:
         self.player_two_score = player_two_score
 
     def point_won_by(self, player_name):
-        pass
+        if player_name == self.player_one_name:
+            self.player_one_score['game'] = self.player_one_score['game'] + 1
+        elif player_name == self.player_two_name:
+            self.player_two_score['game'] = self.player_two_score['game'] + 1
+
+        if self.winner():
+            self.update_set_score()
 
     def update_set_score(self):
-        pass
+        if self.player_one_name == self.get_player_having_highest_score():
+            self.player_one_score['set'] = self.player_one_score['set'] + 1
+        else:
+            self.player_two_score['set'] = self.player_two_score['set'] + 1
+        self.player_one_score['game'] = 0
+        self.player_two_score['game'] = 0
 
     def score(self):
-        pass
+        if self.deuce():
+            print(f'{self.get_set_score()}, Deuce')
+            return
+
+        if self.advantage():
+            score = f'Advantage - {self.get_player_having_highest_score()}'
+            print(f'{self.get_set_score()}, {score}')
+            return
+
+        print(f'{self.get_set_score()}, {self.get_game_score()}')
 
     def get_game_score(self):
-        return '1 - 1'
+        player_one_game_score = score_mapper.get(self.player_one_score['game'])
+        player_two_game_score = score_mapper.get(self.player_two_score['game'])
+        return f'{player_one_game_score} - {player_two_game_score}'
 
     def get_set_score(self):
-        return '2 - 2'
+        return \
+            f'{self.player_one_score["set"]} - {self.player_two_score["set"]}'
 
     def deuce(self):
-        return True
+        return self.player_one_score['game'] >= 3 and \
+         self.player_two_score['game'] == self.player_one_score['game']
 
     def advantage(self):
+        if self.player_one_score['game'] >= 4 and \
+           self.player_one_score['game'] == self.player_two_score['game'] + 1:
+            return True
+        if self.player_two_score['game'] >= 4 and \
+           self.player_two_score['game'] == self.player_one_score['game'] + 1:
+            return True
         return False
 
     def winner(self):
+        if self.player_two_score['game'] >= 4 and \
+           self.player_two_score['game'] >= self.player_one_score['game'] + 2:
+            return True
+        if self.player_one_score['game'] >= 4 and \
+           self.player_one_score['game'] >= self.player_two_score['game'] + 2:
+            return True
         return False
 
     def get_player_having_highest_score(self):
-        return self.player_one_name
-
+        if self.player_one_score['game'] > self.player_two_score['game']:
+            return self.player_one_name
+        else:
+            return self.player_two_name
--- tests/test_match.py
+++ tests/test_match.py
@@ -3,29 +3,127 @@ import pytest
 from tennis.match import Match
 
 
-def test_point_won_by():
-    pass
+@pytest.mark.parametrize(
+    'initial_score, score_after_winning',
+    [
+        ({'set': 0, 'game': 0}, {'set': 0, 'game': 1}),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 0}),
+        ({'set': 1, 'game': 4}, {'set': 2, 'game': 0}),
+    ]
+)
+def test_point_won_by(initial_score, score_after_winning):
+    match = Match('player1', 'player2', player_one_score=initial_score)
+    match.point_won_by('player1')
+    assert match.player_one_score == score_after_winning
 
 
-def test_deuce():
-    pass
+@pytest.mark.parametrize(
+    'player1_score, player2_score, deuce',
+    [
+        ({'set': 0, 'game': 3}, {'set': 0, 'game': 3}, True),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 4}, True),
+        ({'set': 0, 'game': 2}, {'set': 1, 'game': 4}, False),
+    ]
+)
+def test_deuce(player1_score, player2_score, deuce):
+    match = Match(
+        'player1',
+        'player2',
+        player_one_score=player1_score,
+        player_two_score=player2_score
+    )
+    assert match.deuce() is deuce
 
 
-def test_advantage():
-    pass
+@pytest.mark.parametrize(
+    'player1_score, player2_score, advantage',
+    [
+        ({'set': 0, 'game': 3}, {'set': 0, 'game': 4}, True),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 3}, True),
+        ({'set': 0, 'game': 3}, {'set': 1, 'game': 1}, False),
+    ]
+)
+def test_advantage(player1_score, player2_score, advantage):
+    match = Match(
+        'player1',
+        'player2',
+        player_one_score=player1_score,
+        player_two_score=player2_score
+    )
+    assert match.advantage() is advantage
 
 
-def test_winner():
-    pass
+@pytest.mark.parametrize(
+    'player1_score, player2_score, winner',
+    [
+        ({'set': 0, 'game': 4}, {'set': 0, 'game': 2}, True),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 1}, True),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 3}, False),
+    ]
+)
+def test_winner(player1_score, player2_score, winner):
+    match = Match(
+        'player1',
+        'player2',
+        player_one_score=player1_score,
+        player_two_score=player2_score
+    )
+    assert match.winner() is winner
 
 
-def test_get_player_having_highest_score():
-    pass
+@pytest.mark.parametrize(
+    'player1_score, player2_score, high_score_player',
+    [
+        ({'set': 0, 'game': 4}, {'set': 0, 'game': 2}, 'player1'),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 1}, 'player1'),
+        ({'set': 0, 'game': 1}, {'set': 1, 'game': 2}, 'player2'),
+    ]
+)
+def test_get_player_having_highest_score(
+    player1_score,
+    player2_score,
+    high_score_player
+):
+    match = Match(
+        'player1',
+        'player2',
+        player_one_score=player1_score,
+        player_two_score=player2_score
+    )
+    assert match.get_player_having_highest_score() == high_score_player
 
 
-def test_get_game_score():
-    pass
+@pytest.mark.parametrize(
+    'player1_score, player2_score, game_score',
+    [
+        ({'set': 0, 'game': 4}, {'set': 0, 'game': 2}, '40 - 15'),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 1}, '40 - 10'),
+        ({'set': 0, 'game': 0}, {'set': 1, 'game': 3}, '0 - 30'),
+    ]
+)
+def test_get_game_score(player1_score, player2_score, game_score):
+    match = Match(
+        'player1',
+        'player2',
+        player_one_score=player1_score,
+        player_two_score=player2_score
+    )
+    match.get_game_score() == game_score
 
 
-def test_get_set_score():
-    pass
+@pytest.mark.parametrize(
+    'player1_score, player2_score, set_score',
+    [
+        ({'set': 0, 'game': 4}, {'set': 0, 'game': 2}, '0 - 0'),
+        ({'set': 0, 'game': 4}, {'set': 1, 'game': 1}, '0 - 1'),
+        ({'set': 1, 'game': 0}, {'set': 1, 'game': 3}, '1 - 1'),
+    ]
+)
+def test_get_set_score(player1_score, player2_score, set_score):
+    match = Match(
+        'player1',
+        'player2',
+        player_one_score=player1_score,
+        player_two_score=player2_score
+    )
+    match.get_game_score() == set_score
